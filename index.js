3adcf3138809d3ca263eeef47d77fe660576b845
NeverBehave <gayhub@never.pet>
2020-05-23 15:38:59 -0400

    feat: test server connectivity

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const yaml = require('js-yaml');
-const { readFileSync, writeFileSync } = require('fs');
-const path = require('path')
+const yaml = require("js-yaml");
+const got = require("got");
+const { readFileSync, writeFileSync } = require("fs");
+const path = require("path");
 
 // Get document, or throw exception on error
+// Exit on 1 to stop CI
+let doc = [];
 try {
-    const doc = yaml.safeLoad(readFileSync(path.resolve(__dirname, 'src/list.yml'), 'utf8'));
-    console.log(doc)
-    writeFileSync(path.resolve(__dirname, 'public/list.json'), JSON.stringify(doc));
+  doc = yaml.safeLoad(
+    readFileSync(path.resolve(__dirname, "src/list.yml"), "utf8")
+  );
+  console.log("object read: ", doc);
 } catch (e) {
-    console.error(e);
+  console.error(e);
+  process.exit(1);
 }
+
+// Test server's connectivity
+const responses = doc.map((e) => {
+  return got(e.address).then((res) => {
+    return {
+      address: e.address,
+      response: res.body,
+    };
+  });
+});
+
+// Write file on all success
+Promise.all(responses)
+  .then((values) => {
+    console.log("nice, everyone looks healthy:  ", values);
+    writeFileSync(
+      path.resolve(__dirname, "public/list.json"),
+      JSON.stringify(doc)
+    );
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
